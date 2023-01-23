751f321a65d92910e9080b69ac0900ec088b930f
FlickItUpp <84152232+FlickItUpp@users.noreply.github.com>
2023-01-23 02:31:47 -0600

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './navbar.css';
+
+function NavBar() {
+    return (
+        <header className ={navbarClasses.join(" ")}>
+            <div className="logo">
+                
+            </div>
+            <nav className="navigation">
+                {/* navigation */}
+            </nav>
+        </header>
+    )
+}
+
+
+
+
+
